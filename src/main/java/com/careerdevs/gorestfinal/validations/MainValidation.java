e0200ab6f9baebaa27a2fd123860caa46b6bc4ee
“JazlynL” <lubenjazlyn35@gmail.com>
2022-04-23 15:02:36 -0400

    working through mvp final version GOREST.

--- src/main/java/com/careerdevs/gorestfinal/validations/MainValidation.java
+++ src/main/java/com/careerdevs/gorestfinal/validations/MainValidation.java
@@ -3,6 +3,15 @@ package com.careerdevs.gorestfinal.validations;
 public class MainValidation {
     public static void main(String [] args){
 
+
+        ValidationError testVal = new ValidationError();
+
+
+        //this outputs the message as JSONData.
+        testVal.addError("name", "Name is an invalid input");
+        testVal.addError("age", "Age is an invalid input");
+        testVal.addError("email","email is invalid");
+
     }
 
 
--- src/main/java/com/careerdevs/gorestfinal/validations/ValidationError.java
+++ src/main/java/com/careerdevs/gorestfinal/validations/ValidationError.java
@@ -5,6 +5,11 @@ import java.util.HashMap;
 import java.util.Map;
 
     public class ValidationError {
+        /*
+HashMap in Java is a collection based on Map and consists of key-value pairs.
+A HashMap is denoted by < Key, Value > or < K, V >.
+A HashMap element can be accessed using a Key i.e. we must know the key to access the HashMap element
+        */
         private final HashMap<String, String> errors = new HashMap<>();
 
         public void addError (String key, String errorMsg) {
