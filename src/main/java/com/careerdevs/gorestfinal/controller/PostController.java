b483eb4fb475ef7344ad4eb79b36680d6f52fb6d
“JazlynL” <lubenjazlyn35@gmail.com>
2022-04-24 22:27:03 -0400

    almost completely done with GORESTFINAL.

--- src/main/java/com/careerdevs/gorestfinal/controller/PostController.java
+++ src/main/java/com/careerdevs/gorestfinal/controller/PostController.java
@@ -66,7 +66,80 @@ public class PostController {
 
 
         }
+     // getting all pages with a post request
+
+  /*  @PostMapping ("/uploadall")
+    public ResponseEntity<?> uploadAll (
+            RestTemplate restTemplate
+    ) {
+        try {
+            String url = "https://gorest.co.in/public/v2/users";
+            ResponseEntity<User[]> response = restTemplate.getForEntity(url, User[].class);
+            User[] firstPageUsers = response.getBody();
+            if (firstPageUsers == null) {
+                throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to GET first page of " +
+                        "users from GoREST");
+            }
+            ArrayList<User> allUsers = new ArrayList<>(Arrays.asList(firstPageUsers));
+            HttpHeaders responseHeaders = response.getHeaders();
+            String totalPages = Objects.requireNonNull(responseHeaders.get("X-Pagination-Pages")).get(0);
+            int totalPgNum = Integer.parseInt(totalPages);
+            for (int i = 2; i <= totalPgNum; i++) {
+                String pageUrl = url + "?page=" + i;
+                User[] pageUsers = restTemplate.getForObject(pageUrl, User[].class);
+                if (pageUsers == null) {
+                    throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR, "Failed to GET page " + i  +
+                            " of users from GoREST");
+                }
+                allUsers.addAll(Arrays.asList(firstPageUsers));
+            }
+            //upload all users to SQL
+            userRepository.saveAll(allUsers);
+            return new ResponseEntity<>("Users Created: " + allUsers.size(), HttpStatus.OK);
+        } catch (HttpClientErrorException e) {
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+        } catch (Exception e) {
+            return ApiErrorHandling.genericApiError(e);
+        }
+    }
+*/
+
+
+
+
         // put mapping
+
+    @PutMapping
+
+    public ResponseEntity<?>  updateUser(@RequestBody Post updateUser){
+        try{
+
+            // instead of using false we will be using true to update the user info in our database.
+            ValidationError errors = PostValidation.validatePost(updateUser ,postRepository,true);
+
+            // using this method to see if the errors were checked.
+
+            if(errors.hasError()){
+                throw  new HttpClientErrorException(HttpStatus.BAD_REQUEST, errors.toJSONobject());
+            }
+
+            Post updateNew = postRepository.save(updateUser);
+
+
+            return  new ResponseEntity<>(updateNew,HttpStatus.CREATED);
+
+        }catch(HttpClientErrorException e){
+            return ApiErrorHandling.customApiError(e.getMessage(), e.getStatusCode());
+
+        }
+        catch(Exception e){
+            return ApiErrorHandling.genericApiError(e);
+
+        }
+
+    }
+
+
 
     }
 
--- src/main/java/com/careerdevs/gorestfinal/validations/PostValidation.java
+++ src/main/java/com/careerdevs/gorestfinal/validations/PostValidation.java
@@ -3,12 +3,43 @@ package com.careerdevs.gorestfinal.validations;
 import com.careerdevs.gorestfinal.model.Post;
 import com.careerdevs.gorestfinal.repository.PostRepository;
 
+import java.util.Optional;
+
 public class PostValidation {
 
     public static ValidationError validatePost(Post post, PostRepository postRepo, boolean isUpdating){
 
         ValidationError errors = new ValidationError();
+ // if the boolean is true
+        if (isUpdating) {
+            //if the id is equal to 0
+            if (post.getId() == 0) {
 
+                // Add error message to the error var.
+                errors.addError("Id", "there is no valid id ");
+            } else {
+                /*A container object which may or may not contain a non-null value.
+                If a value is present, isPresent() will return true and get() will return the value.
+                Additional methods that depend on the presence or absence of a contained value are provided,
+                such as orElse() (return a default value if value not present) and ifPresent() (execute a block of code if the value is present).
+                This is a value-based class; use of identity-sensitive operations (including reference equality (==), identity hash code,
+                or synchronization) on instances of Optional may have unpredictable results and should be avoided.*/
+                Optional<Post> foundUser = postRepo.findById(post.getId());
+
+
+            // is Empty is a static modifier of the Optional class, Empty returns an of an empty Optional instance.
+
+            if (foundUser.isEmpty()){
+                errors.addError("id","No User found with the ID " + post.getId());
+              }
+
+            }
+        }
+
+       // fields we are setting for
+        String postTitle = post.getTitle();
+        String postBody = post.getBody();
+        Long postId = post.getId();
         return errors;
 
     }
